e45ad496d51942fa36c5aa860a1952cb202b7825
Magnus <magnus.haw@gmail.com>
2024-02-29 23:30:47 -0800

    added more docstrings

--- arcjetCV/utils/output.py
+++ arcjetCV/utils/output.py
@@ -5,7 +5,28 @@ from arcjetCV.utils.utils import splitfn
 
 
 class NumpyEncoder(json.JSONEncoder):
+    '''
+    JSON encoder for NumPy arrays.
+
+    This class extends the JSONEncoder class to handle NumPy arrays, converting them to lists before encoding.
+
+    Example:
+    ```python
+    encoder = NumpyEncoder()
+    encoded_array = encoder.encode(numpy_array)
+    ```
+
+    Attributes:
+        None
+    '''
+
     def default(self, obj):
+        """
+        Convert NumPy arrays to lists for JSON serialization.
+
+        :param obj: Object to encode.
+        :returns: Encoded object.
+        """
         if isinstance(obj, np.ndarray):
             return obj.tolist()
         if isinstance(obj, np.uint8) or isinstance(obj, np.int32):
@@ -16,20 +37,37 @@ class NumpyEncoder(json.JSONEncoder):
 
 
 class OutputListJSON(list):
-    """Extension of list with write to file function
-        expected to hold dictionary objects corresponding to
-        analysis of individual video frames
+    '''
+    Extension of list with write to file function, expected to hold dictionary objects corresponding to analysis of individual video frames.
 
-        filepath argument must contain low and high index constraints
-        which are delimited by underscores:
-        e.g. myoutput_0_10.json
+    This class extends the list class and provides a method to write its contents to a JSON file. It is designed to hold dictionaries corresponding to the analysis of individual video frames. The filename must contain low and high index constraints delimited by underscores, e.g., "myoutput_0_10.json".
 
     Args:
-        list (list): base list class
-        filepath (string): path for saving file
-    """
+        list (list): Base list class.
+        filepath (str): Path for saving file.
+
+    Example:
+    ```python
+    output_list = OutputListJSON('output.json')
+    output_list.append({'INDEX': 0, 'data': 'some_data'})
+    output_list.write()
+    ```
+
+    Attributes:
+        path (str): Path to the JSON file.
+        folder (str): Directory containing the JSON file.
+        _lock (threading.Lock): Threading lock for thread-safe operations.
+        prefix (list): Prefix extracted from the filename.
+        low_index (int): Low index constraint extracted from the filename.
+        high_index (int): High index constraint extracted from the filename.
+    '''
 
     def __init__(self,path):
+        """
+        Initializes the OutputListJSON object.
+
+        :param path: Path for saving file.
+        """
         super(OutputListJSON,self).__init__()
         self.path=path
         folder, name, _ = splitfn(path)
@@ -42,6 +80,11 @@ class OutputListJSON(list):
         self.high_index = int(namesplit[-1])
 
     def write(self, indent=None):
+        """
+        Writes the list to a JSON file.
+
+        :param indent: Number of spaces for indentation (default=None).
+        """
         with self._lock:
             json_object = json.dumps(self, indent=indent, cls=NumpyEncoder)
             # Writing to sample.json
@@ -50,6 +93,13 @@ class OutputListJSON(list):
             print("Edges output written to", self.path)
 
     def load(self,path,extend=True):
+        """
+        Loads data from a JSON file.
+
+        :param path: Path to the JSON file.
+        :param extend: Whether to extend the current list with the loaded data (default=True).
+        :returns: Loaded data.
+        """
         with self._lock:
             with open(path,'r') as fin:
                 dload = json.load(fin)
@@ -58,6 +108,11 @@ class OutputListJSON(list):
                 return dload
 
     def append(self,obj):
+        """
+        Appends an object to the list if its INDEX is within the specified range.
+
+        :param obj: Object to append.
+        """
         with self._lock:
             if obj["INDEX"] <= self.high_index and obj["INDEX"] >= self.low_index:
                 super(OutputListJSON,self).append(obj)
