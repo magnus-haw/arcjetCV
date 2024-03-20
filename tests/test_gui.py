1b5a8ed205a01b871d25547ffd774ee0d163bf18
fsemerar <federicosemeraro94@gmail.com>
2024-03-20 08:57:16 -0700

    Added qtbot.wait(3000)

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -90,10 +90,10 @@ def test_toggle_display_shock_checkbox(app, qtbot):
     app.ui.checkBox_display_shock.setChecked(True)
     initial_state = app.ui.checkBox_display_shock.isChecked()
     qtbot.mouseClick(app.ui.checkBox_display_shock, Qt.LeftButton)
-    QApplication.processEvents()
+    qtbot.wait(3000)
     assert app.ui.checkBox_display_shock.isChecked() != initial_state
     qtbot.mouseClick(app.ui.checkBox_display_shock, Qt.LeftButton)
-    QApplication.processEvents()
+    qtbot.wait(3000)
     assert app.ui.checkBox_display_shock.isChecked() == initial_state
 
 
@@ -413,10 +413,10 @@ def test_toggle_display_shock_2(app, qtbot, mocker):
     app.ui.checkBox_display_shock2.setChecked(True)
     initial_state = app.ui.checkBox_display_shock2.isChecked()
     qtbot.mouseClick(app.ui.checkBox_display_shock2, Qt.LeftButton)
-    QApplication.processEvents()
+    qtbot.wait(3000)
     assert app.ui.checkBox_display_shock2.isChecked() != initial_state
     qtbot.mouseClick(app.ui.checkBox_display_shock2, Qt.LeftButton)
-    QApplication.processEvents()
+    qtbot.wait(3000)
     assert app.ui.checkBox_display_shock2.isChecked() == initial_state
 
 
