4c6711380cca9dbba1f0db4fe51d9f5dd032d8b3
Alexandre Quintart <alexandre.quintart@gmail.com>
2025-04-02 11:29:54 +0200

    updated the readme

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ check_linux_dependencies()
 # Define the setup configuration
 setup(
     name="arcjetcv",
-    version="1.1.0.dev3",  # Set the version directly here
+    version="1.1.0.dev4",  # Set the version directly here
     author="arcjetCV team",
     description="Package to process arcjet videos and segment the edge of the shock and of the sample",
     long_description=long_description,
